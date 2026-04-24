df1eec044de98d985137d612073897752b2d2cf3
Esdras Bezerra da Silva <bzr4@icloud.com>
2026-04-24 20:16:57 -0300

    Notifier Provider Cookies

--- lib/src/features/fortune_cookie/presentation/fortune_cookie_list_screen.dart
+++ lib/src/features/fortune_cookie/presentation/fortune_cookie_list_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod3_playground/src/features/fortune_cookie/domain/fortune_cookie.dart';
+import 'package:riverpod3_playground/src/features/fortune_cookie/presentation/providers/fortune_cookie_provider.dart';
+import 'package:riverpod3_playground/src/features/fortune_cookie/presentation/widgets/fortune_cookie_widget.dart';
 
 class FortuneCookieListScreen extends ConsumerWidget {
   const FortuneCookieListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final cookies = ref.watch(fortuneCookieControllerProvider);
+    final controller = ref.read(fortuneCookieControllerProvider.notifier);
     return Scaffold(
       appBar: AppBar(title: const Text('Fortune Cookies')),
-      body: Center(
-        child: Text('Uma coleção de frase de biscoitos da sorte aqui.'),
+      body: ListView.builder(
+        itemCount: cookies.length,
+        itemBuilder: (context, index) {
+          final cookie = cookies[index];
+          return FortuneCookieWidget(fortuneCookie: cookie);
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => controller.add(FortuneCookie.random()),
         child: const Icon(Icons.add),
       ),
     );
